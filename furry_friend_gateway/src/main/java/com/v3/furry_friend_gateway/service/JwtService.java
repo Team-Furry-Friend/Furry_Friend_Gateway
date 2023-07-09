104d9c62763a9962d5962b69c769651662fb365c
youngsoosoo <lovepys01@naver.com>
2023-07-09 21:41:38 +0900

    토큰 내 정보 추가

--- furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/service/JwtService.java
+++ furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/service/JwtService.java
@@ -1,11 +1,14 @@
 package com.v3.furry_friend_gateway.service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,35 +19,46 @@ import com.v3.furry_friend_gateway.service.dto.response.JwtResponse;
 @Log4j2
 public class JwtService {
 
+    private final String type = "Bearer ";
+
     @Value("${jwt.secretKey}")
     private String key;
 
     // 토큰의 유효성 검사 메서드
     public JwtResponse validateToken(String token) {
         try {
-            // accessToken 파싱 및 memberId 추출
-            Claims claims = Jwts.parser()
-                .setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody();
+            Optional<Claims> result = parse(key, token);
 
-            Object object = claims.get("memberId"); // memberId 추출
-            JwtResponse jwtResponse = null;
+            Long memberId = null;
+            String name = null;
 
-            if (object instanceof Long) {
-                jwtResponse = new JwtResponse((Long) object);
-            } else if (object instanceof Integer) {
-                jwtResponse = new JwtResponse(((Integer) object).longValue());
-            } else {
-                log.warn(claims.get("memberId"));
+            if (result.isPresent()){
+                Claims claims = result.get();
+                memberId = claims.get("memberId", Long.class);
+                name = claims.get("name", String.class);
             }
 
-            return jwtResponse;
+            return JwtResponse.builder()
+                .memberId(memberId)
+                .memberName(name).build();
         } catch (SignatureException e) {
             // 토큰이 유효하지 않으면 false를 반환
             log.error("SignatureException: " + e);
             return null;
         }
+    }
+
+    // 토큰의 유효성 검증
+    public Optional<Claims> parse(String key, String token) {
+        try {
+            return Optional.of(Jwts.parser().setSigningKey(key.getBytes()).parseClaimsJws(untype(token)).getBody());
+        } catch (JwtException e) {
+            return Optional.empty();
+        }
+    }
+
+    private String untype(String token) {
+        return token.substring(type.length());
     }
 
 }
--- furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/service/dto/response/JwtResponse.java
+++ furry_friend_gateway/src/main/java/com/v3/furry_friend_gateway/service/dto/response/JwtResponse.java
@@ -1,13 +1,14 @@
 package com.v3.furry_friend_gateway.service.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@Builder
 public class JwtResponse {
 
-    private final Long memberId;
+    private Long memberId;
+    private String memberName;
 }
